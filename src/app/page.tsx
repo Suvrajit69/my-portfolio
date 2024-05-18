4050683c060f83075cca26f90fc3fac63639d10f
Suvrajit Mondal <suvrajitmondal359@gmail.com>
2024-05-18 19:17:19 +0530

    Hero section done!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 const page = () => {
   return (
-    <main className="bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">Hello!</div>
+    <main className="bg-black-100 flex justify-center items-center flex-col overflow-hidden sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Hero/>
+      </div>
     </main>
   );
 };
