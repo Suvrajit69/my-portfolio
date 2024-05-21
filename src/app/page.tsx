85d5e25e77dc878cbd3782fed05c365435bef63e
Suvrajit Mondal <suvrajitmondal359@gmail.com>
2024-05-21 18:11:32 +0530

    grid about section done!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Grid  from "../components/Grid";
+import RecentProjects from "../components/RecentProjects";
 
 const page = () => {
   return (
     <main className="bg-black-100 flex justify-center items-center flex-col overflow-hidden sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <Hero/>
+        <Hero />
+        <Grid />
+        <RecentProjects/>
       </div>
     </main>
   );
