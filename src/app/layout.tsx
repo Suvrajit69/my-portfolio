eee794f29afd99977857277cf06072a0f6c5f1df
Suvrajit Mondal <suvrajitmondal359@gmail.com>
2024-05-22 17:12:26 +0530

    navbar done!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
             defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
-          >
+          >      
             {children}
           </ThemeProvider>
         </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import Hero from "../components/Hero";
 import Grid  from "../components/Grid";
 import RecentProjects from "../components/RecentProjects";
+import { FloatingNav } from "../components/ui/FloatingNavbar";
+import { navItems } from "../data";
 
 const page = () => {
   return (
     <main className="bg-black-100 flex justify-center items-center flex-col overflow-hidden sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+      <FloatingNav navItems={navItems}/>
         <Hero />
         <Grid />
         <RecentProjects/>
